ca7970b31bbaa8b88dfcd0ea92d6d2a18191a07d
Sch1mith <165226331+Sch1mith@users.noreply.github.com>
2024-09-12 20:15:22 -0300

    Delete Workspace directory

--- Workspace/src/view/main.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $login = $_POST['nome'];
-    $senha = $_POST['senha'];
-    $login_correto = 'admin';
-    $senha_correta = 'admin123';
-    if ($login == $login_correto && $senha == $senha_correta){
-        echo 'seja bem vindo';
-    }
-?>
